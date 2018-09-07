a3800f88338271327534661839d38ac59a4aeb48
Eero Kuurne <eero.kuurne@helsinki.fi>
2018-09-07 10:42:57 +0300

    Test slider

--- bundle.js
+++ bundle.js
@@ -34791,4 +34791,15 @@ function handleOrientation(event) {
 
 window.addEventListener('deviceorientation', handleOrientation);
 
+/* Slider */
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
 },{"jquery":1,"tone":2}]},{},[3]);
